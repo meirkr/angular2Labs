147856f2ed8cbd4ed9d37807389489c38369fe33
ohad <ohads@sela.co.il>
2016-12-08 13:42:08 +0200

    step6

---
+++ step6.2/src/app/playlist/ellipsis.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: "ellipsis"
+})
+
+export class EllipsisPipe implements PipeTransform {
+    // adds ... if the string length > lettersLength
+    transform(val:string, lettersLength:number) {
+        return val.length > lettersLength ? val.substring(0, lettersLength) + '...':  val;
+    }
+}
