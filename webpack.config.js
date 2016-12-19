dbc1addfa4ced9114d602b1f566c8903c2817691
nirnoy <noynir@gmail.com>
2016-12-19 08:15:39 +0200

    Moved to global webpack and node_modules

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+var webpack = require('webpack');
+var path = require('path');
+var webpackMerge = require('webpack-merge');
+
+console.log(JSON.stringify(process.env.npm_package_config_step));
+// Webpack Config
+var root = path.join(__dirname,'step'+process.env.npm_package_config_step);
+var webpackConfig = {
+  entry: {
+    'main': root+'/src/main.browser.ts',
+  },
+
+  output: {
+    publicPath: '',
+    path: path.resolve(root, 'dist'),
+
+  },
+
+  plugins: [
+    new webpack.ContextReplacementPlugin(
+      // The (\\|\/) piece accounts for path separators in *nix and Windows
+      /angular(\\|\/)core(\\|\/)src(\\|\/)linker/,
+      path.resolve(root, './src'),
+      {
+        // your Angular Async Route paths relative to this root directory
+      }
+    ),
+  ],
+
+  module: {
+    loaders: [
+      // .ts files for TypeScript
+      {
+        test: /\.ts$/,
+        loaders: [
+          'awesome-typescript-loader',
+          'angular2-template-loader',
+          'angular2-router-loader'
+        ]
+      },
+      { test: /\.css$/, loaders: ['to-string-loader', 'css-loader'] },
+      { test: /\.html$/, loader: 'raw-loader' }
+    ]
+  }
+
+};
+
+
+// Our Webpack Defaults
+var defaultConfig = {
+  devtool: 'source-map',
+
+  output: {
+    filename: '[name].bundle.js',
+    sourceMapFilename: '[name].map',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  resolve: {
+    extensions: [ '.ts', '.js' ],
+    modules: [ path.resolve(__dirname, 'node_modules') ]
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    watchOptions: { aggregateTimeout: 300, poll: 1000 },
+    contentBase: path.resolve(root, './src')
+  },
+
+  node: {
+    global: true,
+    crypto: 'empty',
+    __dirname: true,
+    __filename: true,
+    process: true,
+    Buffer: false,
+    clearImmediate: false,
+    setImmediate: false
+  }
+};
+
+
+module.exports = webpackMerge(defaultConfig, webpackConfig);
